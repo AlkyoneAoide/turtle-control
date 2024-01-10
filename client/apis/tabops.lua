599ce8fb7e0afa313414a32cda7aaf83427719e8
AlkyoneAoide <playinggameswithfriends@gmail.com>
2024-01-09 18:53:22 -0700

    this is an api not a program :sob:

--- client/apis/tabops.lua
+++ client/apis/tabops.lua
@@ -1,11 +1,8 @@
 -- Adds the values of tab2 to tab1
 function tableConcat(tab1, tab2)
-    if ((not type(arg[1]) == "table") or (not type(arg[2]) == "table")) then
+    if ((not type(tab1) == "table") or (not type(tab2) == "table")) then
         error("Args 1 and 2 should be tables.")
     end
-
-    tab1 = arg[1]
-    tab2 = arg[2]
 
     for i=1, #tab2, 1 do
         tab1[#tab1 + 1] = tab2[i]
